bbfa3dc02ebf8a875473e26d25560623d22dea10
exprodev <k191115@nu.edu.pk>
2023-04-13 02:46:18 +0500

    Assignment05 Commit

---
+++ Assignment04/data_persistant_first_app_tutorial/README.md
@@ -0,0 +1,3 @@
+# first_app_tutorial
+
+A new Flutter project.
---
+++ Assignment04/data_persistant_first_app_tutorial/android/app/src/main/kotlin/com/example/first_app_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_app_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
